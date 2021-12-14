27b454613108955d1124c9fbff0f8a261c5400e1
Term-inator <1121745627@qq.com>
2021-12-14 21:51:41 +0800

    first(X)

--- convertor.py
+++ convertor.py
@@ -1,15 +1,26 @@
-EPS = ''  # 空
-END = '$'  # 串末
+import copy
+
+EPS = ''  # epsilon
+END = '$'  # end of a string
 
 
 class Symbol:
     def __init__(self, val: str, is_terminal: bool):
+        """
+        :param val: value(terminal) or name(not terminal)
+        :param is_terminal: True if is a terminal
+        """
         self.val = val
         self.is_terminal = is_terminal
 
 
 class ProductionRule:
     def __init__(self, id: int, left: Symbol, right: list):  # id, Symbol, list of Symbols
+        """
+        :param id: sequence number
+        :param left: Symbol on the left side of ->
+        :param right: Symbols on the right side of ->
+        """
         self.id = id  # A -> abB
         self.left = left  # A
         self.right = right  # [a, b, B]
@@ -29,6 +40,9 @@ def getSymbol(val: str) -> Symbol:
 
 
 def haveDirectEmptyProductionRule(x: Symbol) -> bool:
+    """
+    whether x has a production rule x -> epsilon
+    """
     assert not x.is_terminal
     x_production_rule_set = production_rules[x]
     for x_production_rule in x_production_rule_set:
@@ -42,33 +56,52 @@ def initFirst() -> None:
         first[symbol] = set()
 
 
-def first() -> None:
+def showFirst() -> None:
+    for k in first:
+        if k.val == EPS:
+            print('EPS', end=' ')
+        print(k.val, end=' [ ')
+        for f in first[k]:
+            if f == EPS:
+                print('EPS', end=' ')
+                continue
+            print(f, end=' ')
+        print(']')
+
+
+def generateFirst() -> None:
     initFirst()
     while True:
-        unchanged = True
+        unchanged = True  # repeat until no more elements are added to any first set
         for symbol in symbols:
             if symbol.is_terminal:
                 if symbol.val not in first[symbol]:
                     unchanged = False
                 first[symbol].add(symbol.val)
-            elif haveDirectEmptyProductionRule(symbol.val):
+            elif haveDirectEmptyProductionRule(symbol):
                 if EPS not in first[symbol]:
                     unchanged = False
                 first[symbol].add(EPS)
-            elif not symbol.is_terminal:
-                all_EPS = True
+            elif not symbol.is_terminal:  # X -> Y1 Y2 ... Yk
                 for production_rule in production_rules[symbol]:
-                    for i in range(len(production_rule.right)):
-                        if len(first[symbol] & first[production_rule.right[i]]) != len(first[symbol]):
+                    all_EPS = True  # if forall i from 1 to k, first(Yi) contains epsilon, then add epsilon to first(X)
+                    for right in production_rule.right:  # Yi
+                        first_right = copy.deepcopy(first[right])  # deepcopy first(Yi) as first_Yi
+                        try:
+                            first_right.remove(EPS)  # remove epsilon from first_Yi
+                        except KeyError:
+                            pass
+                        if len(first[symbol] | first_right) != len(first[symbol]):  # | means Union
                             unchanged = False
-                        first[symbol] = first[symbol] | first[production_rule.right[i]]
-                        if EPS not in first[production_rule.right[i]]:
+                        first[symbol] = first[symbol] | first_right
+                        if EPS not in first[right]:  # whether first(Yi) contains epsilon
                             all_EPS = False
                             break
-                if all_EPS:
-                    if EPS not in first[symbol]:
-                        unchanged = False
-                    first(symbol).add(EPS)
+
+                    if all_EPS:
+                        if EPS not in first[symbol]:
+                            unchanged = False
+                        first[symbol].add(EPS)
 
         if unchanged:
             break
@@ -85,7 +118,7 @@ def initProductionRules() -> None:
         production_rules[symbol] = set()
 
 
-def showProductionRules():
+def showProductionRules() -> None:
     for k in production_rules:
         print(k.val, end=' -> ')
         for i, production_rule in enumerate(production_rules[k]):
@@ -96,10 +129,10 @@ def showProductionRules():
                 print(right.val, end=' ')
             if i < len(production_rules[k]) - 1:
                 print(' | ', end='')
-        print('\n')
+        print()
 
 
-def readSymbols():
+def readSymbols() -> None:
     global terminals, n_terminals
     with open('terminals.txt') as f:
         for line in f.readlines():
@@ -117,7 +150,7 @@ def readSymbols():
         symbols.add(Symbol(n_terminal, False))
 
 
-def readProductionRules():
+def readProductionRules() -> None:
     initProductionRules()
 
     with open('production_rules.txt') as f:
@@ -141,8 +174,9 @@ def readProductionRules():
 def main():
     readSymbols()
     readProductionRules()
-    showProductionRules()
+    # showProductionRules()
+    generateFirst()
+    showFirst()
 
 
 main()
-
